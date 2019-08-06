ce05f4e1ed59a9cdbe69258e467d6215219b14f4
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-06 12:05:33 -0500

    if a client request a url, server sends a response

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 let http = require("http");
 
-let server = http.createServer((req, res)=> {
-  res.end("Hello and welcome to my website");
-})
+let server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end("Hello and welcome to my website");
+  } else if (req.url === '/about'){
+    res.end("Thanks for wanting to know about us");
+  } else {
+    res.end('I cannot find the page you are looking for')
+  }
+});
 server.listen(3000);
